d6112e0496e7754f145c044644e155279b5c56b9
Divya <dipsbhati6@gmail.com>
2018-12-27 18:07:44 -0600

    Main frame of app set up

--- app.py
+++ app.py
@@ -1,29 +1,28 @@
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import Required, AnyOf
 from flask_navigation import Navigation
-from algo import *       
+import pandas as pd    
+from flask_moment import Moment
+from algo import *   
 
 
 app = Flask(__name__)
 nav = Navigation(app)
 app.config['SECRET_KEY'] = 'reallyreallyreallyreallysecretkey'
 
-
-manager   = Manager(app)
 bootstrap = Bootstrap(app)
-moment    = Moment(app)
+
 #choices for length in form
 
-class NameForm(FlaskForm):
+class name_form(FlaskForm):
     #Validators check if the two tickers are in our list of tickers traded on nasdaq, amex, and nyse
     #Also require for every field to be filled out
-    name1   = StringField(u'Name 1:',    validators=[Required(), AnyOf(symbols(), message=u'Not a valid name!')])
-    name2   = StringField(u'Name 2:',    validators=[Required(), AnyOf(symbols(), message=u'Not a valid name!')])
+    name1   = StringField(u'Name 1: ',    validators=[Required()])
+    name2   = StringField(u'Name 2: ',    validators=[Required()])
     submit    = SubmitField(u'Submit')
 
 #Home page
@@ -31,13 +30,13 @@ class NameForm(FlaskForm):
 def index():
     name1  = None
     name2  = None
-    form   = NameForm()
+    form   = name_form()
     if form.validate_on_submit():
         name1  = form.name1.data
         name2  = form.name2.data
 
-        return render_template('index.html', name1=name1, name2=name2)
-    return render_template('index.html', name1=name1, name2=name2)
+        return render_template('index.html', form=form, name1=name1, name2=name2)
+    return render_template('index.html', form=form, name1=name1, name2=name2)
 
 #Run app
 if __name__ == '__main__':

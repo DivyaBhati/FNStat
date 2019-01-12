9924ba215904d7df945369afc1627da3c00c0be3
Divya <dipsbhati6@gmail.com>
2019-01-11 19:32:04 -0600

    Added BG img with minor adjustments

--- get_stats.py
+++ get_stats.py
@@ -12,15 +12,17 @@ def get_player_stats(playername):
 	playerresponse = requests.get(playerurl, headers = headers)
 	playerinfo = playerresponse.json()['lifeTimeStats']
 	#playerstats = list: matches played, wins, win%, kills, k/d
-	playerstats = []
-	for i in range(7,12):
-		playerstats.append(playerinfo[i]['value'])
+	playerstats = [int(playerinfo[7]['value']), int(playerinfo[8]['value']), int(playerinfo[9]['value'][:-1]), int(playerinfo[10]['value']), float(playerinfo[11]['value'])]
+
 	
 	#recents = list of K/D ratio through last 10 sessions
 	recents = []
 	for i in playerresponse.json()['recentMatches']:
 		kd = i['kills'] / i['matches']
 		recents.append(round(kd, 2))
+
+	for i in range(len(recents), 10):
+		recents.append(0)
 
 	#wins: [solos, duos, squads]
 	stats = playerresponse.json()['stats']
